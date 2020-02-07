664210301e08b223a4a3c9ff5002d0a182578eb6
Ali-kiani-dev <54432212+Ali-kiani-dev@users.noreply.github.com>
2020-02-07 11:25:11 +0330

    add updateproduct method and other to resolve/+r

--- app/models/category.js
+++ app/models/category.js
@@ -6,7 +6,6 @@ const Category = Schema({
     name : { type : String, required : true},
     label : { type : String},
     parent : { type : Schema.Types.ObjectId, ref : 'Category'},
-    level : { type : String, required : true, default : 'L1'}
 }, {
     timestamps : true,
     toJSON : { virtuals : true}
